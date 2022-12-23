c79e00a92e6ba3c76f45953c74a99aa81d48e5b7
Linda Lynch <97634723+lindmlynch@users.noreply.github.com>
2022-12-23 00:10:51 +0000

    Add files via upload

---
+++ storeFileFB.py
@@ -0,0 +1,41 @@
+import firebase_admin
+from firebase_admin import credentials, firestore, storage, db
+import os
+
+cred=credentials.Certificate('./serviceAccountKey.json')
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'birdpi-35dd0.appspot.com', # REPLACE WITH YOUR BUCKET ID
+    'databaseURL': 'https://birdpi-35dd0-default-rtdb.europe-west1.firebasedatabase.app/'
+})
+
+bucket = storage.bucket()
+
+ref = db.reference('/')
+home_ref = ref.child('file')
+
+def store_file(fileLoc):
+
+    filename=os.path.basename(fileLoc)
+
+    # Store File in FB Bucket
+    blob = bucket.blob(filename)
+    outfile=fileLoc
+    blob.upload_from_filename(outfile)
+
+def push_db(fileLoc, time):
+
+  filename=os.path.basename(fileLoc)
+
+  # Push file reference to image in Realtime DB
+  home_ref.push({
+      'image': filename,
+      'timestamp': time}
+  )
+
+if __name__ == "__main__":
+    f = open("./test.txt", "w")
+    f.write("a demo upload file to test Firebase Storage")
+    f.close()
+    store_file('./test.txt')
+    push_db('./test.txt', '12/11/2020 9:00' )
+
